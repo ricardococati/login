694a6148f4982a7d9ef92e910f079b7dfcd5c33d
Ricardo Cocati <ricardo.cocati@gmail.com>
2017-10-15 12:53:18 -0200

    Correção das classes inserindo funcionalidade de login

--- src/main/java/com/ricardococati/SpringBootFirstWebApplication.java
+++ src/main/java/com/ricardococati/SpringBootFirstWebApplication.java
@@ -1,4 +1,4 @@
-package com.ricardococati.login;
+package com.ricardococati;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,9 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.ricardococati")
-public class LoginApplication {
+public class SpringBootFirstWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LoginApplication.class, args);
+		SpringApplication.run(SpringBootFirstWebApplication.class, args);
 	}
 }
